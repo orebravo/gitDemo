2565c4b1818dc41525d22307e60cc096bcf49a26
Alexander <orebravo@gmail.com>
2022-03-28 20:40:36 -0500

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AppiumTest1() {
+		System.out.println("AppiumTest1");		
+	}
+	
+	
+	@Test
+	public void AppiumTest2() {
+		System.out.println("AppiumTest2");		
+	}
+
+}
---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void RestApiTest1() {
+		System.out.println("RestApiTest1");		
+	}
+	
+	
+	@Test
+	public void RestApiTest2() {
+		System.out.println("RestApiTest2");		
+	}
+
+}
